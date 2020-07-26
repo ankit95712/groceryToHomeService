d646985c5ea2264a86f919bf7acf14a46881f1bb
Kvivek <vivit1006117@gmail.com>
2020-07-26 17:53:01 +0530

    Vivek | Add spring boot application

---
+++ src/main/java/com/groceryToHome/GroceryToHomeApplication.java
@@ -0,0 +1,13 @@
+package com.groceryToHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryToHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryToHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groceryToHome/GroceryToHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groceryToHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryToHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
